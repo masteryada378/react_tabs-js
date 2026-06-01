1c5ae28dd372fd08237a0f3efb90449b110d09cf
masteryada378 <masteryada378@gmail.com>
2026-06-01 15:40:04 +0300

    fix tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 
 export const Tabs = ({ tabs = [], activeTabId, onTabSelected }) => {
-  const currentTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  const isTabActive = tabId => {
+    const hasActiveTab = tabs.some(tab => tab.id === activeTabId);
+
+    if (hasActiveTab) {
+      return tabId === activeTabId;
+    }
+
+    return tabs.length > 0 && tabId === tabs[0].id;
+  };
+
+  const currentTab = tabs.find(tab => isTabActive(tab.id));
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              className={tab.id === activeTabId ? 'is-active' : ''}
-              data-cy="Tab"
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={e => {
-                  e.preventDefault();
-                  onTabSelected(tab.id);
-                }}
+          {tabs.map(tab => {
+            const active = isTabActive(tab.id);
+
+            return (
+              <li
+                key={tab.id}
+                className={active ? 'is-active' : ''}
+                data-cy="Tab"
               >
-                {tab.title}
-              </a>
-            </li>
-          ))}
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={event => {
+                    event.preventDefault();
+                    if (!active) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
 
